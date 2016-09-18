0bcac2833110a5601d0495b00cb3ee821387c39e
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-09-18 18:07:40 +0800

    Added Question 4

---
+++ src/BeanGrinder/Week1/Ground4.java
@@ -0,0 +1,27 @@
+package BeanGrinder.Week1;
+
+/**
+ * Created by HanWei on 18/9/2016.
+ */
+public class Ground4 {
+    // Week 1 Homework Question 4 - Hailstone Sequence Counter
+    public static void main(String[] args) {
+        System.out.println(Counter(1));
+        System.out.println(Counter(21));
+        System.out.println(Counter(999));
+    }
+
+    public static int Counter(int n){
+        int count = 0;
+        while (n != 1){
+            if (n%2 == 0){
+                n /= 2;
+            }
+            else{
+                n = 3 * n + 1;
+            }
+            count += 1;
+        }
+        return count;
+    }
+}
