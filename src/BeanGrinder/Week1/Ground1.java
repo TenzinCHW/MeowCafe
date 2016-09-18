ba10aeb8546f6649757e749accf97a2d778b59c0
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-09-18 17:38:36 +0800

    Added call to function in main

--- src/BeanGrinder/Week1/Ground1.java
+++ src/BeanGrinder/Week1/Ground1.java
@@ -7,7 +7,7 @@ public class Ground1 {
     // This is homework for week 1
     // Question 1 - Birth Year Generator
     public static void main(String[] args) {
-
+        calYearOfBirth(2013, 8);
     }
 
     public static int calYearOfBirth(int yearCurrent, int age){
