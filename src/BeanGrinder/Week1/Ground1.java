4972a8cb6af3bc1e1918fd853361e40325027f63
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-09-18 17:16:07 +0800

    Updated Week 1 Homework Question 1

--- src/BeanGrinder/Week1/Ground1.java
+++ src/BeanGrinder/Week1/Ground1.java
@@ -4,4 +4,14 @@ package BeanGrinder.Week1;
  * Created by HanWei on 18/9/2016.
  */
 public class Ground1 {
+    // This is homework for week 1
+    // Question 1 - Birth Year Generator
+    public static void main(String[] args) {
+
+    }
+
+    public static int calYearOfBirth(int yearCurrent, int age){
+        return yearCurrent - age;
+    }
+
 }
