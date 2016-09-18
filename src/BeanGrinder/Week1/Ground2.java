906ab9b09a9724885b7d603304ffcff627da9eb7
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-09-18 17:39:15 +0800

    Used encapsulation to calculate power

--- src/BeanGrinder/Week1/Ground2.java
+++ src/BeanGrinder/Week1/Ground2.java
@@ -4,4 +4,32 @@ package BeanGrinder.Week1;
  * Created by HanWei on 18/9/2016.
  */
 public class Ground2 {
+    public static void main(String[] args) {
+        System.out.println(printNums());
+    }
+
+    public static String printNums(){
+        String meow = "";
+        for (int i = 0; i < 8; i++) {
+            meow += power(2, i) + " ";
+        }
+        return meow;
+    }
+
+    public static int power(int base, int power) {
+
+        if (power == 0) {
+            return 1;
+        }
+        else if (power == 1){
+            return base;
+        }
+        else {
+            int total = base;
+            for (int i = 1; i < power; i++) {
+                total *= base;
+            }
+            return total;
+        }
+    }
 }
