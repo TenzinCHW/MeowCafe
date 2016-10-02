98c793cf1353767f9a6121cf189a850e62b29fdf
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-10-02 16:17:44 +0800

    Added Week 3 qn a

--- src/BeanGrinder/Week3/PiGenerator.java
+++ src/BeanGrinder/Week3/PiGenerator.java
@@ -13,7 +13,6 @@ public class PiGenerator {
      */
 
     public static int[] computePiInHex(int precision) {
-        // TODO: Implement (Problem b)
         if (precision<0){
             return null;
         }
@@ -35,7 +34,6 @@ public class PiGenerator {
      * @return a^b mod m
      */
     public static int powerMod(int a, int b, int m) {
-        // TODO: Implement (Problem a)
         if (a < 0 || b < 0 || m <= 0) {
             return -1;
         }
