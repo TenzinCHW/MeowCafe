5fcbb8ae38d7425a08b8ec0adf3436f138e71a02
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-10-09 23:25:50 +0800

    Starting code for Week 5 homework

---
+++ src/BeanGrinder/Week4/inttest.java
@@ -0,0 +1,10 @@
+package BeanGrinder.Week4;
+
+/**
+ * Created by HanWei on 8/10/2016.
+ */
+public class inttest {
+    public static void main(String[] args) {
+        int meow = Integer.parseInt("hi");
+    }
+}
