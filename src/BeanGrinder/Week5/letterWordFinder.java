f97df875297b4853340a03a589aff37b9e1d9b36
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2016-10-17 01:11:22 +0800

    Week 5 homework question 8b

---
+++ src/BeanGrinder/Week5/letterWordFinder.java
@@ -0,0 +1,43 @@
+package BeanGrinder.Week5;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by HanWei on 17/10/2016.
+ */
+public class letterWordFinder {
+    public static void main(String[] args) {
+        System.out.println(returnWords("left", "this stressful time on the twelfth felt strangely uneventful"));
+    }
+
+    public static ArrayList<String> returnWords(String word, String sentence){
+        String[] wordsInSent = sentence.split("\\s");
+        ArrayList<String> wordsFound = new ArrayList<String>();
+
+        findWords(word, wordsInSent, wordsFound);
+        return wordsFound;
+    }
+
+    public static void findWords(String word, String[] allWords, ArrayList<String> found){
+        if (allWords.length == 1 && wordMatch(word, allWords[0])){
+                found.add(allWords[0]);
+                return;
+        }else{
+            if (wordMatch(word, allWords[0])) {
+                found.add(allWords[0]);
+            }
+            findWords(word, Arrays.copyOfRange(allWords, 1, allWords.length), found);
+        }
+    }
+
+    public static boolean wordMatch(String in1, String in2) {
+        int grr = 0;
+        for (int i = 0; i < in1.length(); i++) {
+            if (in2.contains(in1.substring(i, i+1))){
+                grr += 1;
+            }
+        }
+        return grr == in1.length();
+    }
+}
